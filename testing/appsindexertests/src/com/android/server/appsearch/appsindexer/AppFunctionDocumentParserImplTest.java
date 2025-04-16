573b91761edde345810cdf934d141689f9f6dedc
Alex Saveliev <alexsav@google.com>
2025-04-16 21:23:45 +0000

    Removes non-incremental update and all related methods
    Bug: 367410454
    Flag: com.android.appsearch.flags.enable_apps_indexer_incremental_put
    Test: AppsIndexerImplTest
    Test: AppSearchHelperTest
    Change-Id: I1dad758c3acaf0fe887294deda5fd4eb759a98fa

--- testing/appsindexertests/src/com/android/server/appsearch/appsindexer/AppFunctionDocumentParserImplTest.java
+++ testing/appsindexertests/src/com/android/server/appsearch/appsindexer/AppFunctionDocumentParserImplTest.java
@@ -28,7 +28,6 @@ import android.content.res.Resources;
 import androidx.annotation.NonNull;
 
 import com.android.server.appsearch.appsindexer.appsearchtypes.AppFunctionDocument;
-import com.android.server.appsearch.appsindexer.appsearchtypes.AppFunctionStaticMetadata;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -44,7 +43,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -105,167 +103,6 @@ public class AppFunctionDocumentParserImplTest {
     private void setXmlInput(String xml) throws IOException {
         InputStream inputStream = new ByteArrayInputStream(xml.getBytes());
         when(mAssetManager.open(TEST_XML_ASSET_FILE_PATH)).thenReturn(inputStream);
-    }
-
-    @Test
-    public void parse_singleAppFunctionWithAllProperties() throws Exception {
-        setXmlInput(
-                "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n"
-                        + "<version>1</version>\n"
-                        + "<appfunctions>\n"
-                        + "  <appfunction>\n"
-                        + "    <function_id>com.example.utils#print</function_id>\n"
-                        + "    <schema_name>insert_note</schema_name>\n"
-                        + "    <schema_version>1</schema_version>\n"
-                        + "    <schema_category>utils</schema_category>\n"
-                        + "    <enabled_by_default>false</enabled_by_default>\n"
-                        + "    <restrict_callers_with_execute_app_functions>true\n"
-                        + "</restrict_callers_with_execute_app_functions>\n"
-                        + "    <display_name_string_res>10</display_name_string_res>\n"
-                        + "  </appfunction>\n"
-                        + "</appfunctions>");
-
-        List<AppFunctionStaticMetadata> appFunctions =
-                mParser.parse(mPackageManager, TEST_PACKAGE_NAME, TEST_XML_ASSET_FILE_PATH);
-
-        assertThat(appFunctions).hasSize(1);
-
-        AppFunctionStaticMetadata appFunction1 = appFunctions.get(0);
-        assertThat(appFunction1.getFunctionId()).isEqualTo("com.example.utils#print");
-        assertThat(appFunction1.getPackageName()).isEqualTo(TEST_PACKAGE_NAME);
-        assertThat(appFunction1.getSchemaName()).isEqualTo("insert_note");
-        assertThat(appFunction1.getSchemaVersion()).isEqualTo(1);
-        assertThat(appFunction1.getSchemaCategory()).isEqualTo("utils");
-        assertThat(appFunction1.getEnabledByDefault()).isEqualTo(false);
-        assertThat(appFunction1.getRestrictCallersWithExecuteAppFunctions()).isEqualTo(true);
-        assertThat(appFunction1.getDisplayNameStringRes()).isEqualTo(10);
-    }
-
-    @Test
-    public void parse_singleAppFunctionWithDefaults() throws Exception {
-        setXmlInput(
-                "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n"
-                        + "<version>1</version>\n"
-                        + "<appfunctions>\n"
-                        + "  <appfunction>\n"
-                        + "    <function_id>com.example.utils#print</function_id>\n"
-                        + "  </appfunction>\n"
-                        + "</appfunctions>");
-
-        List<AppFunctionStaticMetadata> appFunctions =
-                mParser.parse(mPackageManager, TEST_PACKAGE_NAME, TEST_XML_ASSET_FILE_PATH);
-
-        assertThat(appFunctions).hasSize(1);
-
-        AppFunctionStaticMetadata appFunction1 = appFunctions.get(0);
-        assertThat(appFunction1.getFunctionId()).isEqualTo("com.example.utils#print");
-        assertThat(appFunction1.getPackageName()).isEqualTo(TEST_PACKAGE_NAME);
-        assertThat(appFunction1.getSchemaName()).isNull();
-        assertThat(appFunction1.getSchemaVersion()).isEqualTo(0);
-        assertThat(appFunction1.getSchemaCategory()).isNull();
-        assertThat(appFunction1.getEnabledByDefault()).isEqualTo(true);
-        assertThat(appFunction1.getRestrictCallersWithExecuteAppFunctions()).isEqualTo(false);
-        assertThat(appFunction1.getDisplayNameStringRes()).isEqualTo(0);
-    }
-
-    @Test
-    public void parse_ignoresUnknownProperties() throws Exception {
-        setXmlInput(
-                "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n"
-                        + "<version>1</version>\n"
-                        + "<appfunctions>\n"
-                        + "  <appfunction>\n"
-                        + "    <function_id>com.example.utils#print</function_id>\n"
-                        + "    <parameters><name>test</name></parameters>\n"
-                        + "    <unknown_property>test</unknown_property>\n"
-                        + "  </appfunction>\n"
-                        + "</appfunctions>");
-
-        List<AppFunctionStaticMetadata> appFunctions =
-                mParser.parse(mPackageManager, TEST_PACKAGE_NAME, TEST_XML_ASSET_FILE_PATH);
-
-        assertThat(appFunctions).hasSize(1);
-        // Only contain known properties from XML or properties populated by default.
-        assertThat(appFunctions.get(0).getPropertyNames())
-                .containsExactly(
-                        "functionId",
-                        "packageName",
-                        "enabledByDefault",
-                        "mobileApplicationQualifiedId");
-    }
-
-    @Test
-    public void parse_missingFunctionId() throws Exception {
-        setXmlInput(
-                "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n"
-                        + "<version>1</version>\n"
-                        + "<appfunctions>\n"
-                        + "  <appfunction>\n"
-                        + "    <schema_name>insert_note</schema_name>\n"
-                        + "    <schema_version>1</schema_version>\n"
-                        + "    <schema_category>utils</schema_category>\n"
-                        + "  </appfunction>\n"
-                        + "</appfunctions>");
-
-        List<AppFunctionStaticMetadata> appFunctions =
-                mParser.parse(mPackageManager, TEST_PACKAGE_NAME, TEST_XML_ASSET_FILE_PATH);
-
-        assertThat(appFunctions).isEmpty();
-    }
-
-    @Test
-    public void parse_malformedXml() throws Exception {
-        // Missing </functionId>
-        setXmlInput(
-                "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n"
-                        + "<version>1</version>\n"
-                        + "<appfunctions>\n"
-                        + "  <appfunction>\n"
-                        + "    <function_id>com.example.utils#print"
-                        + "    <schema_name>insert_note</schema_name>\n"
-                        + "    <schema_version>1</schema_version>\n"
-                        + "    <schema_category>utils</schema_category>\n"
-                        + "  </appfunction>\n"
-                        + "</appfunctions>");
-
-        List<AppFunctionStaticMetadata> appFunctions =
-                mParser.parse(mPackageManager, TEST_PACKAGE_NAME, TEST_XML_ASSET_FILE_PATH);
-
-        assertThat(appFunctions).isEmpty();
-    }
-
-    @Test
-    public void parse_exceedMaxNumAppFunctions() throws Exception {
-        mParser =
-                new AppFunctionDocumentParserImpl(
-                        TEST_INDEXER_PACKAGE_NAME,
-                        new TestAppsIndexerConfig() {
-                            @Override
-                            public int getMaxAppFunctionsPerPackage() {
-                                return 2;
-                            }
-                        });
-        setXmlInput(
-                "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n"
-                        + "<version>1</version>\n"
-                        + "<appfunctions>\n"
-                        + "  <appfunction>\n"
-                        + "    <function_id>com.example#send_message1</function_id>\n"
-                        + "  </appfunction>\n"
-                        + "  <appfunction>\n"
-                        + "    <function_id>com.example#send_message2</function_id>\n"
-                        + "  </appfunction>\n"
-                        + "  <appfunction>\n"
-                        + "    <function_id>com.example#send_message3</function_id>\n"
-                        + "  </appfunction>\n"
-                        + "</appfunctions>");
-
-        List<AppFunctionStaticMetadata> appFunctions =
-                mParser.parse(mPackageManager, TEST_PACKAGE_NAME, TEST_XML_ASSET_FILE_PATH);
-
-        assertThat(appFunctions).hasSize(2);
-        assertThat(appFunctions.get(0).getFunctionId()).isEqualTo("com.example#send_message1");
-        assertThat(appFunctions.get(1).getFunctionId()).isEqualTo("com.example#send_message2");
     }
 
     @Test
