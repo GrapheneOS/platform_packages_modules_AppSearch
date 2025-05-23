8941c092f90e71ca6fc2efd1728d28fc2ace3200
Caroline Braviak <cbraviak@google.com>
2025-05-23 16:09:18 +0000

    AppSearch/ISS: Set Isolated Storage Default Disabled
    Default disable Isolated Storage Service. This is being used for
    carrier builds.
    
    Test: Flash + verify no VMs running
    Test: atest AppSearchSessionCtsTest
    Bug: 419853926
    Flag: com.android.appsearch.flags.enable_isolated_storage
    Change-Id: Ie14c122cbcbc92f47315890a1305b7f9e4d4626c

--- service/java/com/android/server/appsearch/isolated_storage_service/IsolatedStorageServiceManager.java
+++ service/java/com/android/server/appsearch/isolated_storage_service/IsolatedStorageServiceManager.java
@@ -74,7 +74,7 @@ public final class IsolatedStorageServiceManager {
     public static final String SYSTEM_PROPERTY_ENABLE_ISOLATED_STORAGE =
             "ro.appsearch.feature.enable_isolated_storage";
     public static final long DEFAULT_MEMORY_BYTES = 512_000_000;
-    public static final boolean DEFAULT_ISOLATED_STORAGE_ENABLED = true;
+    public static final boolean DEFAULT_ISOLATED_STORAGE_ENABLED = false;
     public static final boolean DEFAULT_ISOLATED_STORAGE_MIGRATION_ENABLED = false;
     private static final String ISOLATED_STORAGE_SERVICE =
             "com.android.appsearch.ISOLATED_STORAGE_SERVICE";
