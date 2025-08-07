1651436df58d11b4a727eb1ba5d7ad01746161c7
Lingyun Zhao <lingyunzhao@google.com>
2025-08-07 11:12:23 -0700

    Revert "ISS: delete VM if isolated_storage is disabled"
    This reverts commit b957141f29ae4933f66f5509a77c0e439e303456.
    
    Reason for revert: b/436815568
    
    Bug: 430643956
    Change-Id: I9667cf9c2c8697187b295c59210772d85f1ee982

--- apk/src/com/android/server/appsearch/isolated_storage_service/IsolatedStorageService.java
+++ apk/src/com/android/server/appsearch/isolated_storage_service/IsolatedStorageService.java
@@ -229,7 +229,7 @@ public class IsolatedStorageService extends Service {
                         TAG,
                         "expected config is not compatible with the running config. Recreating"
                                 + " VM");
-                deleteVmByName(vmm, VM_NAME);
+                deleteVm(vmm, VM_NAME);
                 return vmm.getOrCreate(VM_NAME, config);
             }
             return vm;
@@ -277,7 +277,13 @@ public class IsolatedStorageService extends Service {
     @GuardedBy("mLock")
     private void deleteCurrentVmLocked(@Nullable String vmErrorMessage) {
         Log.i(TAG, "Deleting current VM...");
-        if (deleteVmByName(VM_NAME)) {
+        Context context = createDeviceProtectedStorageContext();
+        VirtualMachineManager vmm = context.getSystemService(VirtualMachineManager.class);
+        if (vmm == null) {
+            Log.e(TAG, "Unable to get VirtualMachineManager");
+            return;
+        }
+        if (deleteVm(vmm, VM_NAME)) {
             if (vmErrorMessage != null) {
                 Log.wtf(TAG, vmErrorMessage);
             } else {
@@ -290,30 +296,17 @@ public class IsolatedStorageService extends Service {
         }
     }
 
-    /** Delete old CE VMs. */
     private void deleteOldVms() {
         VirtualMachineManager vmm = getSystemService(VirtualMachineManager.class);
         if (vmm == null) {
             Log.e(TAG, "Unable to get VirtualMachineManager");
             return;
         }
-        deleteVmByName(vmm, "isolated_storage_service_vm");
-        deleteVmByName(vmm, "isolated_storage_service2_vm");
+        deleteVm(vmm, "isolated_storage_service_vm");
+        deleteVm(vmm, "isolated_storage_service2_vm");
     }
 
-    /** Deletes DE VM by name. */
-    private boolean deleteVmByName(String name) {
-        Context context = createDeviceProtectedStorageContext();
-        VirtualMachineManager vmm = context.getSystemService(VirtualMachineManager.class);
-        if (vmm == null) {
-            Log.e(TAG, "Unable to get VirtualMachineManager");
-            return false;
-        }
-        return deleteVmByName(vmm, name);
-    }
-
-    /** Deletes VM by name. */
-    private boolean deleteVmByName(VirtualMachineManager vmm, String name) {
+    private boolean deleteVm(VirtualMachineManager vmm, String name) {
         try {
             vmm.delete(name);
         } catch (VirtualMachineException e) {
@@ -543,11 +536,6 @@ public class IsolatedStorageService extends Service {
                 throw new RemoteException(e.getMessage());
             }
             return VM_START_STATUS_SUCCESS;
-        }
-
-        @Override
-        public void deleteVm() {
-            deleteVmByName(VM_NAME);
         }
 
         @Override
